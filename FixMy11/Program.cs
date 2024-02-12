f3eba8a24cb62bbafdc96e9ad4b369a7939f61cf
Dan Banfield <73112854+Dan-Banfield@users.noreply.github.com>
2024-02-12 11:37:19 +0000

    Add project files.

---
+++ FixMy11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OSVersionExtension;
+using System.Windows.Forms;
+using OperatingSystem = OSVersionExtension.OperatingSystem;
+
+namespace FixMy11
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            /* Ensure the user is running the program on Windows 11
+             * as it's untested on other Windows versions. */
+
+            if (!(OSVersion.GetOperatingSystem() == OperatingSystem.Windows11))
+            {
+                MessageBox.Show("This application only supports Windows 11 builds. Thank you for your patience.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
+
+            Application.Run(new Form1());
+        }
+    }
+}
