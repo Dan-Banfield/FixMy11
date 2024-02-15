05d2644f6445373f6396221ef8990411bd1e655b
Dan Banfield <73112854+Dan-Banfield@users.noreply.github.com>
2024-02-15 21:33:22 +0000

    Clean up code.

--- FixMy11/Program.cs
+++ FixMy11/Program.cs
@@ -16,12 +16,11 @@ namespace FixMy11
             /* Ensure the user is running the program on Windows 11
              * as it's untested on other Windows versions. */
 
-            if (!(OSVersion.GetOperatingSystem() == OperatingSystem.Windows11))
+            if (OSVersion.GetOperatingSystem() != OperatingSystem.Windows11)
             {
                 MessageBox.Show("This application only supports Windows 11.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Environment.Exit(0);
             }
-
             Application.Run(new MainForm());
         }
     }
